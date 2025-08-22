94455aa833fb7c892dbde748005eb11f3a4147e2
Yunusbek Ismailov <unusismailov5@gmail.com>
2025-08-22 16:00:34 +0500

    fix

--- src/TodoApp.vue
+++ src/TodoApp.vue
@@ -3,23 +3,9 @@ import { ref } from 'vue';
 import TodoComponent from '@/components/TodoComponent.vue';
 
 const todos = ref([
+
   {
     id: 1,
-    text: 'Create Vue 3 Todo App',
-    completed: true,
-  },
-  {
-    id: 2,
-    text: 'Create Apostrophe Webpack configuration module',
-    completed: true,
-  },
-  {
-    id: 3,
-    text: 'Implement the Vue 3 Todo App in Apostrophe',
-    completed: true,
-  },
-  {
-    id: 4,
     text: 'Do the demo',
     completed: false,
   },
@@ -29,7 +15,7 @@ const todos = ref([
 <template>
   <div class="container mx-auto mt-4" data-todo-app>
     <h1 class="p-2 text-center text-3xl font-bold">
-      Vue 3 Todo App with Vite and Typescript
+       Todo App 
     </h1>
     <TodoComponent :todos="todos" />
   </div>
